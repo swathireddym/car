eb4add8c8114b214e3f6f7e69012dbc769a7f695
swathireddym <138190470+swathireddym@users.noreply.github.com>
2023-07-06 09:27:14 +0530

    Update displaycar.c

--- displaycar.c
+++ displaycar.c
@@ -1,3 +1,6 @@
+//The code defines a function displayCars that displays the cars parked in the car park.
+It checks if the car park is empty by checking the head variable. If the car park is empty, it prints a corresponding message. 
+Otherwise, it traverses the linked list of cars and prints the slot number, car number, owner name, and parking time for each car.//
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
