0aed0c60639e148a17dc77e6875380c96189ddf6
swathireddym <138190470+swathireddym@users.noreply.github.com>
2023-07-13 15:44:21 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,21 +2,22 @@
 It allows users to park cars, remove cars, display parked cars, and save/load car park data.
 It presents a menu to the user and performs the corresponding actions based on their choices. 
 The code aims to provide basic functionality for managing a car park.//
-#include "carpark.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "displaycar.c"
-#include "parkcar.c"
-#include "removecar.c"
-#include "savedata.c"
+
+  #include "carpark.h" // Include header file for car park functionality
+#include <stdio.h> // Include standard input/output library
+#include <stdlib.h> // Include standard library
+#include <string.h> // Include string manipulation functions
+#include "displaycar.c" // Include function for displaying parked cars
+#include "parkcar.c" // Include function for parking a car
+#include "removecar.c" // Include function for removing a car
+#include "savedata.c" // Include function for saving data
 
 int main() {
     int choice;
     char carNumber[20];
     char ownerName[50];
 
-    loadData("car_park_data.txt");
+    loadData("car_park_data.txt"); // Load car park data from a file
 
     while (1) {
         printf("1. Park a car\n");
@@ -25,34 +26,34 @@ int main() {
         printf("4. Save data\n");
         printf("5. Exit\n");
         printf("Enter your choice: ");
-        scanf("%d", &choice);
+        scanf("%d", &choice); // Read user's choice from input
 
-        if (choice == 5) {
+        if (choice == 5) { // If the choice is 5, exit the program
             printf("Exiting the program.\n");
             break;
-        } else if (choice < 1 || choice > 5) {
+        } else if (choice < 1 || choice > 5) { // If the choice is invalid, ask for input again
             printf("Invalid choice. Please try again.\n");
             continue;
         }
 
-        switch (choice) {
+        switch (choice) {//switch is used to take control of a particular case
             case 1:
                 printf("Enter the car number: ");
-                scanf("%s", carNumber);
+                scanf("%s", carNumber); // Read the car number from input
 
-                if (atoi(carNumber) < 0) {
+                if (atoi(carNumber) < 0) { // Check if the car number is valid
                     printf("Invalid car number. Please try again.\n");
                     break;
                 }
 
                 printf("Enter the owner's name: ");
                 getchar();
-                fgets(ownerName, sizeof(ownerName), stdin);
-                ownerName[strcspn(ownerName, "\n")] = '\0';
+                fgets(ownerName, sizeof(ownerName), stdin); // Read the owner's name from input
+                ownerName[strcspn(ownerName, "\n")] = '\0'; // Remove the newline character
 
                 int validCarNumber = 1;
                 for (int i = 0; i < strlen(carNumber); i++) {
-                    if (!isalnum(carNumber[i])) {
+                    if (!isalnum(carNumber[i])) { // Check if the car number contains only alphanumeric characters
                         validCarNumber = 0;
                         break;
                     }
@@ -60,16 +61,16 @@ int main() {
 
                 int validOwnerName = 1;
                 for (int i = 0; i < strlen(ownerName); i++) {
-                    if (!isalpha(ownerName[i]) && ownerName[i] != ' ') {
+                    if (!isalpha(ownerName[i]) && ownerName[i] != ' ') { // Check if the owner's name contains only alphabetic characters and spaces
                         validOwnerName = 0;
                         break;
                     }
                 }
 
-                if (!validCarNumber || !validOwnerName) {
+                if (!validCarNumber || !validOwnerName) { // If the input is invalid, display an error message
                     printf("Invalid input. Please try again.\n");
                 } else {
-                    struct Car* parkedCar = parkCar(carNumber, ownerName);
+                    struct Car* parkedCar = parkCar(carNumber, ownerName); // Park the car
                     printf("Car parked successfully in slot number %d.\n", parkedCar->slotNumber);
                 }
 
@@ -77,9 +78,9 @@ int main() {
             case 2:
                 printf("Enter the slot number: ");
                 int slotNumber;
-                scanf("%d", &slotNumber);
+                scanf("%d", &slotNumber); // Read the slot number from input
 
-                int removalStatus = removeCar(slotNumber);
+                int removalStatus = removeCar(slotNumber); // Remove the car from the specified slot number
                 if (removalStatus == 0) {
                     printf("No cars parked in the car park.\n");
                 } else if (removalStatus == -1) {
@@ -88,10 +89,10 @@ int main() {
 
                 break;
             case 3:
-                displayCars();
+                displayCars(); // Display the parked cars
                 break;
             case 4:
-                saveData("car_park_data.txt");
+                saveData("car_park_data.txt"); // Save the car park data to a file
                 break;
             default:
                 break;
@@ -100,3 +101,5 @@ int main() {
 
     return 0;
 }
+      
+               
