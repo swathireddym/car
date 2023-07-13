611d07061b887428b224798c5485d5099cd034cd
swathireddym <138190470+swathireddym@users.noreply.github.com>
2023-07-13 15:57:54 +0530

    Update carpark.h

--- carpark.h
+++ carpark.h
@@ -1,32 +1,32 @@
 //The code defines a header file "carpark.h" that declares structures and functions related to a car park management system. 
 It includes functions for parking a car, removing a car, displaying parked cars, calculating parking time and fees, as well as saving and loading data to/from files. 
 The header file encapsulates the functionality of the car park system for use in other source files.//
-#ifndef CAR_PARK_H
+
+#ifndef CAR_PARK_H // Header guard to prevent multiple inclusions
 #define CAR_PARK_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <ctype.h>
+#include <stdio.h> // Include standard input/output library
+#include <stdlib.h> // Include standard library
+#include <string.h> // Include string manipulation functions
+#include <time.h> // Include time-related functions
+#include <ctype.h> // Include character type functions
 
-#define PARKING_RATE_PER_HOUR 50
+#define PARKING_RATE_PER_HOUR 50 // Define a constant for the parking rate per hour
 
-struct Car {
-    int slotNumber;
-    char carNumber[20];
-    char ownerName[50];
-    time_t timestamp;
-    struct Car* next;
+struct Car { // Define the Car structure
+    int slotNumber; // Slot number of the car
+    char carNumber[20]; // Car number
+    char ownerName[50]; // Owner's name
+    time_t timestamp; // Timestamp of when the car was parked
+    struct Car* next; // Pointer to the next car in the linked list
 };
 
+struct Car* parkCar(const char* carNumber, const char* ownerName); // Function prototype for parking a car
+int removeCar(int slotNumber); // Function prototype for removing a car
+void displayCars(); // Function prototype for displaying parked cars
+double calculateParkingTime(time_t parkedTime); // Function prototype for calculating parking time
+double calculateParkingFee(double parkingTime); // Function prototype for calculating parking fee
+void saveData(const char* filename); // Function prototype for saving car park data to a file
+void loadData(const char* filename); // Function prototype for loading car park data from a file
 
-struct Car* parkCar(const char* carNumber, const char* ownerName);
-int removeCar(int slotNumber);
-void displayCars();
-double calculateParkingTime(time_t parkedTime);
-double calculateParkingFee(double parkingTime);
-void saveData(const char* filename);
-void loadData(const char* filename);
-
-#endif
+#endif // End of header guard
